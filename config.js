0686433ff1ff65a4221baaf0b092ae76d929ced8
UmarStar <umarstar737@gmail.com>
2024-08-17 13:27:47 +0500

    make a change in mongo

--- config.js
+++ config.js
@@ -1,3 +1,15 @@
 const mongoose = require('mongoose');
+require('dotenv').config(); // Load environment variables from .env
 
-mongoose.connect('mongodb+srv://umarstar737:moni922@cluster0.lihyr.mongodb.net/GetData');
+const MONGO_URI = process.env.MONGO_URI; // Access the MONGO_URI from the .env file
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log('MongoDB connected successfully');
+})
+.catch((err) => {
+    console.error('MongoDB connection error:', err);
+});
--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const express =require("express");
-const app =express();
-require("./config");
-const MyModel =require("./schema");
+const express = require('express');
+const app = express();
+require('./config'); // Import the config.js to establish MongoDB connection
 
-const cors =require("cors");   //it is used to show data in json
-
+const MyModel = require('./schema');
+const cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
 
-app.post("/post",async(req ,res)=>{
-    let result =new MyModel(req.body);
-    result =await result.save();
-    res.send(result) 
-})
+app.post('/post', async (req, res) => {
+    let result = new MyModel(req.body);
+    result = await result.save();
+    res.send(result);
+});
 
-app.listen(4000,(err)=>{
-    if(err){
-
-        console.log("error to start")
+app.listen(4000, (err) => {
+    if (err) {
+        console.log('Error starting the server');
+    } else {
+        console.log('Server running on port 4000');
     }
-})
+});
